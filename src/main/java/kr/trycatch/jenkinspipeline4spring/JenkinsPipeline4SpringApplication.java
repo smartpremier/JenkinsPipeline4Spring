1f1e197451bffe2a0ce79e66c9a92d40183a11ef
smartpremier <smartpremier@trycatch.kr>
2022-04-08 21:48:10 +0900

    init project

---
+++ src/main/java/kr/trycatch/jenkinspipeline4spring/JenkinsPipeline4SpringApplication.java
@@ -0,0 +1,13 @@
+package kr.trycatch.jenkinspipeline4spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipeline4SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsPipeline4SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/trycatch/jenkinspipeline4spring/JenkinsPipeline4SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.trycatch.jenkinspipeline4spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipeline4SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
